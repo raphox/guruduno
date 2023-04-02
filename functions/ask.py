85905b0409f17e57441565cb6a40af822ef6f39a
Raphael Araújo <raphox.araujo@gmail.com>
2023-04-02 00:57:15 -0300

    First commit

--- functions/ask.py
+++ functions/ask.py
@@ -4,18 +4,33 @@ from langchain.prompts.chat import (
     SystemMessagePromptTemplate,
     HumanMessagePromptTemplate,
 )
+from langchain.schema import AIMessage
 from query import execute as query_execute
 
-CHAT_PROMPT_TEMPLATE = "Você é o croupier bem humorado. Você é responsável por distribuir as cartas e também participar do jogo. O jogo de cartas é chamado Uno e muito divertido. Os participantes lhe farão perguntas sobre o jogo e você terá acesso as regras relacionadas a pergunta. Responda apenas perguntas relacionadas ao jogo. Para qualquer outra pergunta apenas responda: Infelizmente não posso lhe ajudar com isso.\n\n Regra Relacionadas:\n\n {rules}\n\n Quando a intensão do jogador é realizar uma jogada que vá contras as regras, informe a ele que isso não é permitido."
+CHAT_PROMPT_TEMPLATE = "Você é o croupier bem humorado. Você é responsável por distribuir as cartas e também participar do jogo. O jogo de cartas é chamado Uno e é muito divertido. Os participantes lhe farão perguntas sobre o jogo e você terá acesso as regras relacionadas a pergunta. Responda apenas perguntas relacionadas ao jogo. Para qualquer outra pergunta apenas responda: Infelizmente não posso lhe ajudar com isso.\n\n Regra Relacionadas:\n\n {rules}\n\n Quando a intensão do jogador é realizar uma jogada que vá contras as regras, informe a ele que isso não é permitido."
+
 
 def execute(question):
+    docs = query_execute(question)
+    document, score = [None, 0] if not docs else docs[0]
+
+    if score < 0.8:
+        return AIMessage(content="Infelizmente não posso lhe ajudar com isso.")
+
     chat = ChatOpenAI(temperature=0)
 
-    system_message_prompt = SystemMessagePromptTemplate.from_template(CHAT_PROMPT_TEMPLATE)
-    human_template = "{question}"
-    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
-    chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+    system_message_prompt = SystemMessagePromptTemplate.from_template(
+        CHAT_PROMPT_TEMPLATE
+    )
 
-    docs = query_execute(question)
+    human_template = "{question}"
+
+    human_message_prompt = HumanMessagePromptTemplate.from_template(
+        human_template
+    )
+
+    chat_prompt = ChatPromptTemplate.from_messages(
+        [system_message_prompt, human_message_prompt]
+    )
 
     return chat(chat_prompt.format_prompt(rules=docs, question=question).to_messages())
--- functions/main.py
+++ functions/main.py
@@ -2,6 +2,7 @@ import firebase_admin
 
 from firebase_admin import firestore
 from ask import execute as ask_execute
+from query import upsert_question, execute as query_execute
 
 def answer_question(event, context):
     """Triggered by a change to a Firestore document.
@@ -20,8 +21,19 @@ def answer_question(event, context):
     affected_doc = client.collection(collection_path).document(document_path)
 
     question = event["value"]["fields"]["title"]["stringValue"]
+    similarity_search = query_execute(question, k=1, namespace="questions")
+
+    if similarity_search:
+        document, score = similarity_search[0]
+
+        if score > 0.95:
+            print(document.metadata['answer'])
+            return
+
     answer = ask_execute(question)
 
+    upsert_question(question, answer.content)
+
     affected_doc.update({
-        u'answer': answer
+        u'answer': answer.content
     })
