763846c4f0d41d0fa1658446e853985861726c6d
Raphael Araújo <raphox.araujo@gmail.com>
2023-04-03 11:46:40 -0300

    First commit

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,31 @@
 const functions = require("firebase-functions");
 const request = require("request");
 
+require("dotenv").config();
+
 exports.answerQuestion = functions.firestore
-  .document("/questions/{questionId}")
-  .onCreate((snap, context) => {
-    const data = snap.data();
+    .document("/questions/{questionId}")
+    .onCreate((snap, context) => {
+      const data = snap.data();
 
-    // Access the parameter `{documentId}` with `context.params`
-    functions.logger.log(
-      "Answering question",
-      context.params.documentId,
-      data.title
-    );
+      // Access the parameter `{documentId}` with `context.params`
+      functions.logger.log(
+          "Answering question",
+          context.params.documentId,
+          data.title,
+      );
 
-    request("http://www.google.com", (error, response, body) => {
-      console.error("error:", error); // Print the error if one occurred
-      console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-      console.log("body:", body); // Print the HTML for the Google homepage.
+      const options = {
+        url: "https://guruduno.onrender.com/answer_question",
+        headers: {
+          Authorization: `Bearer ${process.env.SECURITY_TOKEN}`,
+        },
+        formData: {
+          event: data,
+          collection: "questions",
+          document: context.params.documentId,
+        },
+      };
+
+      request.post(options);
     });
-  });
