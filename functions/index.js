da0f56ddaf6409173f9f641ebd3978dcc453332d
Raphael Araújo <raphox.araujo@gmail.com>
2023-04-03 13:36:59 -0300

    First commit

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,4 @@
 const functions = require("firebase-functions");
-const request = require("request");
 
 require("dotenv").config();
 
@@ -8,24 +7,21 @@ exports.answerQuestion = functions.firestore
     .onCreate((snap, context) => {
       const data = snap.data();
 
-      // Access the parameter `{documentId}` with `context.params`
       functions.logger.log(
           "Answering question",
-          context.params.documentId,
+          context.params.questionId,
           data.title,
       );
 
-      const options = {
-        url: "https://guruduno.onrender.com/answer_question",
+      fetch("https://guruduno.onrender.com/answer_question", {
+        method: "post",
+        body: JSON.stringify({
+          event: data,
+          collection: "questions",
+          document: context.params.questionId,
+        }),
         headers: {
           Authorization: `Bearer ${process.env.SECURITY_TOKEN}`,
         },
-        formData: {
-          event: data,
-          collection: "questions",
-          document: context.params.documentId,
-        },
-      };
-
-      request.post(options);
+      });
     });
