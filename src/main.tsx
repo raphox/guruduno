2b58e5f24815f61a88f66a9cd520d581ab4e4050
Raphael Araújo <raphox.araujo@gmail.com>
2023-04-26 09:18:37 -0300

    Using textarea

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
+window.addEventListener("contextmenu", (e) => e.preventDefault());
+
 const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
